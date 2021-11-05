af6524a1f02b68bfd0c1a6b69572a6e874f76b98
Eduardo Gonzalez <eduar2gp@gmail.com>
2021-11-05 18:16:00 -0400

    product-detail route added

--- Connection/src/app/app-routing.module.ts
+++ Connection/src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { ProductComponent } from './component/product/product.component';
 import { ProductListComponent } from './component/product-list/product-list.component';
 import { BenefitComponent } from './component/benefit/benefit.component';
 import { FeatureComponent } from './component/feature/feature.component';
+import { UseCaseComponent } from './component/use-case/use-case.component';
+import { ProductDetailComponent } from './component/product-detail/product-detail.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'product', component: ProductComponent },
-  { path: 'benefit', component: BenefitComponent },
-  { path: 'feature', component: FeatureComponent },
+  { path: 'product-detail', component: ProductDetailComponent },
 ];
 
 @NgModule({
