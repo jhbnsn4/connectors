179358a26173b7c8cd89d23b76654d231ed5ce8a
Eduardo Gonzalez <eduar2gp@gmail.com>
2021-11-07 16:25:30 -0500

    new nav bar structure, responsive side nav, visible till display width <= 800 px (as per Kienen suggestion)

--- Connection/src/app/component/nav-bar/nav-bar.component.ts
+++ Connection/src/app/component/nav-bar/nav-bar.component.ts
@@ -1,25 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map, shareReplay } from 'rxjs/operators';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { UserService } from 'src/app/service/user.service';
+import { ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
 export class NavBarComponent implements OnInit {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-  .pipe(
-    map(result => result.matches),
-    shareReplay()
-  );
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  constructor(private breakpointObserver: BreakpointObserver, public userService: UserService) { }
+  constructor(
+    private observer: BreakpointObserver,
+    public userService: UserService
+  ) {
+
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+  }
 }
